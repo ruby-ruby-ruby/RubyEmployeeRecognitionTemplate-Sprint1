b7405a598ee8b31dd48d4507fb8c7da0a9dfa782
Jakub Arent <jakub.arent+ruby@gmail.com>
2022-04-24 22:14:03 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1.upto(5) do |i|
+    Employee.create(email: "employee#{i}@test.com", password: 'password')
+end
